7c3d5b00cec4c0e16075e156b373510ad77c9591
gayhub <gayhub@live.cn>
2018-07-18 22:14:33 +0800

    init

--- src/main/java/cn/itbat/springboot/admin/SpringBootAdminApplication.java
+++ src/main/java/cn/itbat/springboot/admin/SpringBootAdminApplication.java
@@ -3,12 +3,14 @@ package cn.itbat.springboot.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Spring Boot Admin
  *
  * @author log.r
  */
+@Configuration
 @SpringBootApplication
 @EnableAdminServer
 public class SpringBootAdminApplication {
