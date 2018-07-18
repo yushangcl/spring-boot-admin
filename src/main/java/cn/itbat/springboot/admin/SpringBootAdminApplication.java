48b8ea0bae93bac46e9b5e3fa0b5ca4249df6332
log.r <log.r@live.cn>
2018-07-18 18:22:15 +0800

    init

---
+++ src/main/java/cn/itbat/springboot/admin/SpringBootAdminApplication.java
@@ -0,0 +1,19 @@
+package cn.itbat.springboot.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Admin
+ *
+ * @author log.r
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
